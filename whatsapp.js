d8580d1d315d215da2e16bc58d80d7c6ae16583f
curious harish <36083887+Harish-Uthayakumar@users.noreply.github.com>
2019-12-19 22:48:48 +0530

    Create whatsapp.js

---
+++ whatsapp.js
@@ -0,0 +1,3 @@
+var elem = document.getElementById("ref");
+	var currentlocation = window.location.href;
+	elem.setAttribute("href", "http://web.whatsapp.com/send?phone=917709090319&text=Hey, I was just checking out the following product "+currentlocation+"%0a%0aDo you have any special offers on it?%0a%0aThis request was generated using WhatsApp tool by Huloq");
